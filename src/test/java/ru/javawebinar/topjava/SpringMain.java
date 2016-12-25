4432f9a3d719709baf578d4fd4a9033421c63319
Mikhailo Skafenko <misha41192@mail.ru>
2016-12-25 12:37:44 +0100

    hw3_1_add_spring_config_for_repository.mock

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -19,10 +19,10 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-test-inMemory.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(UserTestData.USER);
+            adminUserController.update(UserTestData.USER, UserTestData.USER_ID);
             System.out.println();
 
             MealRestController mealController = appCtx.getBean(MealRestController.class);
