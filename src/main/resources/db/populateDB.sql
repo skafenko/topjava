12aa31372403062bef03bcae398ad04fab1beed3
Mikhailo Skafenko <misha41192@mail.ru>
2016-12-25 16:49:21 +0100

    hw3_2_init_populate_table_meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -11,3 +12,14 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+  (100000, make_timestamp(2015, 5, 30, 10, 0, 0), 'Завтрак', 500),
+  (100000, make_timestamp(2015, 5, 30, 13, 0, 0), 'Обед', 1000),
+  (100000, make_timestamp(2015, 5, 30, 20, 0, 0), 'Ужин', 500),
+  (100000, make_timestamp(2015, 5, 31, 10, 0, 0), 'Завтрак', 1000),
+  (100000, make_timestamp(2015, 5, 31, 13, 0, 0), 'Обед', 500),
+  (100000, make_timestamp(2015, 5, 31, 20, 0, 0), 'Ужин', 510),
+  (100001, make_timestamp(2015, 6, 1, 14, 0, 0), 'Админ ланч', 510),
+  (100001, make_timestamp(2015, 6, 1, 21, 0, 0), 'Админ ужин', 1500);
+
