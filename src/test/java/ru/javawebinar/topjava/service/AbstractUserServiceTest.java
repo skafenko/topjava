dbaf298203451a8a464b4178ac393218142d086e
Mikhailo Skafenko <misha41192@mail.ru>
2017-01-24 22:00:14 +0100

    hw6_2_fix_jdbc_test

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -3,7 +3,9 @@ package ru.javawebinar.topjava.service;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.DataAccessException;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.JpaUtil;
@@ -21,13 +23,21 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     @Autowired
     protected UserService service;
 
-    @Autowired
+    @Autowired(required = false)
     protected JpaUtil jpaUtil;
+
+    @Autowired
+    private Environment environment;
 
     @Before
     public void setUp() throws Exception {
         service.evictCache();
-        jpaUtil.clear2ndLevelHibernateCache();
+        for (String profile : environment.getActiveProfiles()) {
+            if (Profiles.DATAJPA.equals(profile) || Profiles.JPA.equals(profile)) {
+                jpaUtil.clear2ndLevelHibernateCache();
+                break;
+            }
+        }
     }
 
     @Test
