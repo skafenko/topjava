b19498875697dbd475f4dd48806d113af271b013
Mikhailo Skafenko <misha41192@mail.ru>
2017-01-25 22:55:10 +0100

    hw6_4_impl_MealController_instead_MealServlet

---
+++ src/main/java/ru/javawebinar/topjava/web/MealController.java
@@ -0,0 +1,83 @@
+package ru.javawebinar.topjava.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.DateTimeUtil;
+import ru.javawebinar.topjava.web.meal.AbstractMealController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+/**
+ * Created by Mykhailo on 24.01.2017.
+ */
+@Controller
+public class MealController extends AbstractMealController {
+
+    @RequestMapping(value = "/meals", method = RequestMethod.GET)
+    public String meals(Model model) {
+        model.addAttribute("meals", getAll());
+        return "meals";
+    }
+
+    @RequestMapping(value = "/meals", params = "action=filter", method = RequestMethod.POST)
+    public String filter(Model model, HttpServletRequest request) {
+        LocalDate startDate = DateTimeUtil.parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = DateTimeUtil.parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = DateTimeUtil.parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = DateTimeUtil.parseLocalTime(request.getParameter("endTime"));
+        model.addAttribute("meals", getBetween(startDate, startTime, endDate, endTime));
+        return "meals";
+    }
+
+    @RequestMapping(value = "/meals", method = RequestMethod.POST)
+    public String save(HttpServletRequest request) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        final Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.valueOf(request.getParameter("calories")));
+
+        if (request.getParameter("id").isEmpty()) {
+            create(meal);
+        } else {
+            update(meal, getId(request));
+        }
+        return "redirect:meals";
+    }
+
+    @RequestMapping(value = "/meals", params = "action=delete", method = RequestMethod.GET)
+    public String delete(HttpServletRequest request) {
+        int id = getId(request);
+        delete(id);
+        return "redirect:meals";
+    }
+
+    @RequestMapping(value = "/meals", params = "action=create", method = RequestMethod.GET)
+    public String create(Model model) {
+        final Meal meal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+        model.addAttribute("meal", meal);
+        return "meal";
+    }
+
+    @RequestMapping(value = "/meals", params = "action=update", method = RequestMethod.GET)
+    public String update(Model model, HttpServletRequest request) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        final Meal meal = get(getId(request));
+        model.addAttribute("meal", meal);
+        return "meal";
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.valueOf(paramId);
+    }
+}
