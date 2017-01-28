9254b838c95f3a8e59059ae64fa25003095ab11f
Mikhailo Skafenko <misha41192@mail.ru>
2017-01-28 15:06:57 +0100

    hw7_2_imp_ResourceControllerTest

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by Mykhailo on 28.01.2017.
+ */
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testStyleCSS() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("text/css"));
+    }
+}
